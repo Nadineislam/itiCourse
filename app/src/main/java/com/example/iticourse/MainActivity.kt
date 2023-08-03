450a6a9598245aa0a0cbb8dc64fdd995601dd3f4
Nadine Islam <nadeen.kamaal@gmail.com>
2023-08-03 14:50:46 +0300

    Gender and Sports

--- app/src/main/java/com/example/iticourse/MainActivity.kt
+++ app/src/main/java/com/example/iticourse/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.iticourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.iticourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnLogin.setOnClickListener { val name=binding.etUserName.text.toString()
+            val selectedGender = when (binding.radioGroup.checkedRadioButtonId) {
+                binding.rbFemale.id -> "Female"
+                binding.rbMale.id -> "Male"
+                else -> ""
+            }
+            val selectedSports = mutableListOf<String>()
+            if (binding.cbFootball.isChecked) {
+                selectedSports.add(binding.cbFootball.text.toString())
+            }
+            if (binding.cbTennis.isChecked) {
+                selectedSports.add(binding.cbTennis.text.toString())
+            }
+            if (binding.cbRunning.isChecked) {
+                selectedSports.add(binding.cbRunning.text.toString())
+            }
+
+        Toast.makeText(baseContext,"Hello $name ,you're $selectedGender ,and your sports are ${selectedSports.joinToString()}",Toast.LENGTH_LONG).show()}
 
     }
 }

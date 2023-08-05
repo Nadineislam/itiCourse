a9a4187ba4305273bf4d915abeddd3aecdd2bbfb
Nadine Islam <nadeen.kamaal@gmail.com>
2023-08-05 16:28:11 +0300

    Explicit and Implicit Intents

--- app/src/main/java/com/example/iticourse/MainActivity.kt
+++ app/src/main/java/com/example/iticourse/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.iticourse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -9,9 +10,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnLogin.setOnClickListener { val name=binding.etUserName.text.toString()
+        binding.btnLogin.setOnClickListener {
+            val name = binding.etUserName.text.toString()
             val selectedGender = when (binding.radioGroup.checkedRadioButtonId) {
                 binding.rbFemale.id -> "Female"
                 binding.rbMale.id -> "Male"
@@ -28,7 +30,23 @@ class MainActivity : AppCompatActivity() {
                 selectedSports.add(binding.cbRunning.text.toString())
             }
 
-        Toast.makeText(baseContext,"Hello $name ,you're $selectedGender ,and your sports are ${selectedSports.joinToString()}",Toast.LENGTH_LONG).show()}
+            // Toast.makeText(baseContext,"Hello $name ,you're $selectedGender ,and your sports are ${selectedSports.joinToString()}",Toast.LENGTH_LONG).show()
+            val intent = Intent(baseContext, SecondActivity::class.java)
+            intent.putExtra("Name", name)
+            intent.putExtra("Gender", selectedGender)
+            intent.putExtra("Sports", selectedSports.joinToString())
+            startActivityForResult(intent, 100)
+        }
+
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100) {
+            Toast.makeText(baseContext, data?.getStringExtra("LOGIN_BY"), Toast.LENGTH_LONG).show()
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/iticourse/SecondActivity.kt
@@ -0,0 +1,36 @@
+package com.example.iticourse
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.iticourse.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val name = intent.getStringExtra("Name")
+        val gender = intent.getStringExtra("Gender")
+        val sports = intent.getStringExtra("Sports")
+        Toast.makeText(
+            baseContext,
+            "Hello $name you're $gender and your sports are $sports",
+            Toast.LENGTH_LONG
+        ).show()
+        binding.btnClick.setOnClickListener {
+            val loginBy = if (binding.rbGoogle.isChecked) {
+                "Google"
+            } else {
+                "Facebook"
+            }
+            val intent = Intent()
+            intent.putExtra("LOGIN_BY", loginBy)
+            setResult(2, intent)
+            finish()
+        }
+
+    }
+}

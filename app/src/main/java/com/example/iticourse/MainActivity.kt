2a15773abc4b67b351033ee420eef2e6720fa1b9
Nadine Islam <nadeen.kamaal@gmail.com>
2023-08-06 14:56:51 +0300

    Menu And Dialog

--- app/src/main/java/com/example/iticourse/MainActivity.kt
+++ app/src/main/java/com/example/iticourse/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.iticourse
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.iticourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -47,6 +50,32 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(baseContext, data?.getStringExtra("LOGIN_BY"), Toast.LENGTH_LONG).show()
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.goToSecondActivity -> {
+                startActivity(Intent(baseContext, SecondActivity::class.java))
+            }
+            R.id.exit -> {
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setMessage("Are you sure to exit?")
+                    .setPositiveButton("yes") { _, _ ->
+                        finish()
+                    }.setNegativeButton("No") { dialog, _ ->
+                        dialog.cancel()
+                    }
+                val dialog = dialogBuilder.create()
+                dialog.show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
